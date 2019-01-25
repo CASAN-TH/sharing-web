f4a25e5633db581018252d2d11dbd480d1364c59
KrittinTch <petesuper256@gmail.com>
2019-01-25 15:06:17 +0700

    click to detail

--- src/app/pages/donate-detail/donate-detail.component.ts
+++ src/app/pages/donate-detail/donate-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DonateDetailService } from 'src/app/services/donate-detail/donate-detail.service';
+import { ActivatedRoute } from '@angular/router';
 
 
 @Component({
@@ -11,12 +12,24 @@ export class DonateDetailComponent implements OnInit {
 
   data: any;
   imageArray: any;
+  itemId:any;
 
   constructor(
     private donateDetailService: DonateDetailService,
+    private activatedRoute: ActivatedRoute,
   ) { }
 
   async ngOnInit() {
+
+    this.activatedRoute
+      .queryParams
+      .subscribe(params =>{
+        if (params['id']){
+          this.itemId = params['id'];
+          console.log(this.itemId);
+        }
+        });
+    
     this.data = await this.donateDetailService.getDonateDetail();
     this.imageArray = this.data.data.images
     console.log(this.data)
