38f294665df3f64c6e50cb57510db5c43dede975
pure1234567a <pure1234567a@gmail.com>
2019-01-29 14:44:27 +0700

    service me

---
+++ src/app/services/me/me.service.ts
@@ -0,0 +1,64 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.prod';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MeService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  private authorizationHeader() {
+    const token = window.localStorage.getItem('token@sharing-web-dev');
+    // console.log(token);
+    const headers = new HttpHeaders().set('Authorization', 'Bearer ' + token);
+    return headers;
+  }
+
+  getProfile() {
+    return this.http.get(environment.apiUrl + '/api/me', { headers: this.authorizationHeader() }).toPromise();
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
