8fe49810cc26463ef873bf09a75a80597ca8190d
pure1234567a <pure1234567a@gmail.com>
2019-01-30 15:35:15 +0700

    modal detail

--- src/app/pages/donate-detail/donate-detail.component.ts
+++ src/app/pages/donate-detail/donate-detail.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { DonateDetailService } from 'src/app/services/donate-detail/donate-detail.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NgxSpinnerService } from "ngx-spinner";
+import { MatDialogRef, MatDialog, MAT_DIALOG_DATA } from '@angular/material';
 
 
 @Component({
@@ -21,26 +22,29 @@ export class DonateDetailComponent implements OnInit {
     private activatedRoute: ActivatedRoute,
     private router: Router,
     private spinner: NgxSpinnerService,
+    private dialogRef: MatDialogRef<DonateDetailComponent>,
+    public dialog: MatDialog,
+    @Inject(MAT_DIALOG_DATA) public modalData: any,
   ) { }
 
   ngOnInit() {
-    this.spinner.show();
-    this.activatedRoute
-      .queryParams
-      .subscribe(params => {
-        if (params['id'] && params['user_id']) {
-          this.itemId = params['id'];
-          this.userId = params['user_id']
-        }
-      });
+    // this.spinner.show();
+    // this.activatedRoute
+    //   .queryParams
+    //   .subscribe(params => {
+    //     if (params['id'] && params['user_id']) {
+    //       this.itemId = params['id'];
+    //       this.userId = params['user_id']
+    //     }
+    //   });
     this.getData();
-
+    console.log(this.modalData);
   }
 
   async getData() {
     try {
       let idProd = {
-        id: this.itemId
+        id: this.modalData._id
       }
       this.data = await this.donateDetailService.getDetail(idProd);
       this.imageArray = this.data.data.image;
@@ -53,14 +57,21 @@ export class DonateDetailComponent implements OnInit {
 
   async onAcceptDonate() {
     let body = {
-      product_id: this.itemId,
-      user_id: this.userId
+      product_id: this.modalData._id,
+      user_id: this.modalData.user_id
     }
     this.spinner.show();
-    const res: any = await this.donateDetailService.updateStatus(body);
-    console.log(res);
-    this.router.navigate(['home']);
-    this.spinner.hide();
+    try {
+      const res: any = await this.donateDetailService.updateStatus(body);
+      console.log(res);
+      // this.router.navigate(['home']);
+      this.dialogRef.close();
+      this.spinner.hide();
+      this.spinner.hide();
+    } catch (error) {
+      this.dialogRef.close();
+      this.spinner.hide();
+    }
   }
 
 }
