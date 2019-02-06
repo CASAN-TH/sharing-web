e212b12e7e07af1113b6e569f3fdad2d80759c44
Natsulelou <nutshapon_lelou@hotmail.com>
2019-02-06 14:30:30 +0700

    introduce ui

--- src/app/pages/introduce/introduce.component.ts
+++ src/app/pages/introduce/introduce.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { DonateServiceService } from 'src/app/services/donate-servic/donate-service.service';
+import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-introduce',
@@ -6,10 +10,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./introduce.component.scss']
 })
 export class IntroduceComponent implements OnInit {
+  constructor(
+    private donateService: DonateServiceService,
+    private router: Router,
+    public dialog: MatDialog,
+    private spinner: NgxSpinnerService,
+  ) { }
 
-  constructor() { }
+  data: any;
 
   ngOnInit() {
+    this.spinner.show()
+    this.getDonate()
+  }
+
+  async getDonate() {
+    try {
+      this.data = await this.donateService.getDonate();
+      console.log(this.data)
+      this.spinner.hide();
+    } catch (error) {
+      this.spinner.hide();
+      throw error
+    }
+  }
+
+  openLogin(){
+    this.router.navigate(['login']);
   }
 
 }
